8ccf8bd6a743e4011b5a03970e818e601b6d11ff
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-10 18:38:15 -0400

    Program with defined veriables

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,3 +5,13 @@
 
 /*This program will prompt the user to input their monthly wage, monthly bill payments, and the percentage of the leftover amount they plan to save.  It will output the amount leftover after bills, amount saved, and amount left for spending.*/
 
+//Variables
+var monthlyWage = 0; //Variable for the user's monthly wage
+var monthlyBills = 0; //Variable for the user's monthly bills
+var percentToSave = 0; //Variable for the percentage the user 							wishes to save
+var amtAfterBills = 0; //Variable to store the amount the user has left after paying bills
+var amtAfterSaving = 0; //Variable to store the amount the user will have left after saving money
+var amtSaved = 0; //Variable to store the amount the user is saving
+
+
+
