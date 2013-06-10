1d8a9cc948a3dd86f07510948f94f5764b6e47b6
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-10 18:31:25 -0400

    defined what the program will perform

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,4 +1,7 @@
 /* Name: Andy Becker
-   Date: 
-   Assignment:
+   Date: 6/10/2013
+   Assignment: Expressions_Personal
 */
+
+/*This program will prompt the user to input their monthly wage, monthly bill payments, and the percentage of the leftover amount they plan to save.  It will output the amount leftover after bills, amount saved, and amount left for spending.*/
+
