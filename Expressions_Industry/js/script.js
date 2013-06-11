f6f8305f6acf5a26286c80770ff5cc8605bb6fe2
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-11 12:22:27 -0400

    Updated index.html and script.js
    Changed the title for index.html to reflect the assignment
    Added an explaination of the program to script.js

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,4 +1,7 @@
 /* Name: Andy Becker
-   Date: 
-   Assignment:
+   Date: 6/11/2013
+   Assignment: Expressions: Industry
 */
+
+/*In my current job we support the iOS users and have over 45000 iOS devices deployed in the field.  When ordering devices and scanners that attach to the iPhones we have to know the average number of devies received each day, and the percentage of those devices that need to be replaced, and not just restored and reconfigured.  This program will allow the user to specify what device they are inputting the information for, the number received for each day, and the number that failed for each day.  It will output the results.*/
+
