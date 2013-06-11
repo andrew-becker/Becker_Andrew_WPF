020de6bbbe16dca398c2a0729c319219595b2ea7
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-11 12:41:23 -0400

    Fixed array variables and created input dialog
    Initialized arrays correctly.  Also created input dialogs for the user

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -7,9 +7,19 @@
 
 //Variables
 var deviceName; //String for the user to input the device Name
-var numReceived[]; //Array for the number of devices received each day
-var numReceivedBroken[]; //Array for the number of devices received that are broken
+var numReceived = new Array(); //Array for the number of devices received each day
+var numReceivedBroken = new Array(); //Array for the number of devices received that are broken
 var count = 0; //Counter to move through the Array
 var numDays; //Number of days of information the user will be inputting, user will set this value
 var avgReceived; //Average number of devices received
 var percentBroken; //Percentage of devices that are broken and will need to be replaced
+
+//Input
+deviceName = prompt("Please enter the name of the device. \nEx: iPhone");//Prompts user to input the name of the device
+numDays = prompt("Please enter the number of days you have information for " + deviceName + ".");//Prompts the user to input the number of days they have device totals for that device
+while(count <numDays)//Loops through the input prompts for the number of times the user has information for
+{
+	numReceived[count] = prompt("Enter the total number of " + deviceName + " recieved on day " + (count+1) + ".");//Prompts the user for input and assigns that information to the array
+	numReceivedBroken[count] = prompt("Enter the number of broken " + deviceName + " recieved on day " + (count+1) + ".");//Prompts the user for input and assigns that information to the array
+	count++;//increments the counter by 1
+}
