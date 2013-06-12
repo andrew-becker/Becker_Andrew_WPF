f941fead8e03e641f39327b1f6e81dab89eeeddc
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-12 18:16:05 -0400

    Updated script.js and index.html
    Updated index.html with the correct title.  Also updated the script.js
    file with an explaination of the program and the variables

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,4 +1,12 @@
 /* Name: Andy Becker
-   Date: 
-   Assignment:
+   Date: 6/13/2013
+   Assignment: Expressions - Wacky
 */
+
+/* I have always found it interesting when watching a show like Unwrapped and they talk about how far the number of whatever they create would reach, such as this could go from here to the moon and back.  For this assignment I decided to make a program that will take the name and the length of a product and also how many they make in a year and see if it could reach from my house to Full Sail.*/
+
+//Variables
+const distanceToFullSail = 33897600;//length, in inches, from Full Sail to my house
+var nameOfItem; //variable for the name of the item
+var lengthOfItem; //variable for the length of the item in inches
+var numProduced; //variable for the number of the item produced each year
