eddb7c99e880bd9b300de745d6fbe24bff9b199b
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-11 20:19:11 -0400

    Final revision

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,6 +1,6 @@
 /* Name: Andy Becker
    Date: 6/11/2013
-   Assignment: Expressions: Industry
+   Assignment: Expressions - Industry
 */
 
 /*In my current job we support the iOS users and have over 45000 iOS devices deployed in the field.  When ordering devices and scanners that attach to the iPhones we have to know the average number of devies received each day, and the fail rate of those devices.  This program will allow the user to specify what device they are inputting the information for, the number received for each day, and the number that failed for each day.  It will output the results.*/
