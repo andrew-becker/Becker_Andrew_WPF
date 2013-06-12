04d6a2f1d28bd541583e15772b676fbd7ca8efcc
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-12 18:30:36 -0400

    updated script.js
    Added the calculations to script.js

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -12,9 +12,22 @@ var nameOfItem; //variable for the name of the item
 var lengthOfItem; //variable for the length of the item in inches
 var numProduced; //variable for the number of the item produced each year
 var result; //Variable to hold the result of true or false
+var lengthProducedPerYr; //Variable to hold the length (in inches) of the item produced each year
 
 //Input
 nameOfItem = prompt("What is the name of the item? \nEx. Pencil");//Prompt for the user to input the name of the item
 lengthOfItem = prompt("How long is the item, in inches? \nEx. 10");//Prompt for the user to input the length of the item
 numProduced = prompt("How many " + nameOfItem + "are produced each year?");//Prompt for the user to input the number of the item produced each year
+
+//Calculations
+lengthProducedPerYr = lengthOfItem * numProduced;//Calculates the total length produced each year
+if(distanceToFullSail < lengthProducedPerYr)//looks to see if the distance from my house to Full Sail is shorter than the length of the item produced each year
+{
+	result = true;//sets result to true
+}else//if the disance to Full Sail is more than the total length of the item it will apply the code under else
+{
+	result = false;//sets result to false
+}
+
+
 
