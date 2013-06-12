f2274bb4d4a0bbd369170d1db95e88a0ead02e52
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-11 20:16:11 -0400

    Added the output and fixed two bugs
    Added the parseInt() function to the addition for totalReceived and
    totalReceivedBroken since the numbers were being passed to the addition
    as strings and not as integers

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -13,6 +13,8 @@ var count = 0; //Counter to move through the Array
 var numDays; //Number of days of information the user will be inputting, user will set this value
 var avgReceived = 0; //Average number of devices received
 var percentBroken; //Percentage of devices that are broken and will need to be replaced
+var totalReceived = 0; //Total number of devices recieved
+var totalReceivedBroken = 0; //Total number of devices received broken
 
 //Input
 deviceName = prompt("Please enter the name of the device. \nEx: iPhone");//Prompts user to input the name of the device
@@ -21,34 +23,17 @@ while(count <numDays)//Loops through the input prompts for the number of times t
 {
 	numReceived[count] = prompt("Enter the total number of " + deviceName + " recieved on day " + (count+1) + ".");//Prompts the user for input and assigns that information to the array
 	numReceivedBroken[count] = prompt("Enter the number of broken " + deviceName + " recieved on day " + (count+1) + ".");//Prompts the user for input and assigns that information to the array
+	totalReceived += parseInt(numReceived[count],10); //Add the number recieved to get the total devices received
+	totalReceivedBroken += parseInt(numReceivedBroken[count],10); //Add the number of devies received broken to get the total
 	count++;//increments the counter by 1
 }
 
 //Calculations
-/*Need to add the amounts received and divide it by numDays.  Then need to total the amount broken.  Then divide the total broken by the total received to get the percent broken.*/
+avgReceived = totalReceived / numDays;//divides the total received by the number of days to get the average
 
-//Calculate the average received
-count = 0;//reset count back to zero
-while(count < numDays)//loops the number of days the user entered
-{
-	avgReceived += numReceived[count];//adds each of the numbers received to average received to get the total
-}
-avgReceived /= numDays;//divides the total received by the number of days to get the average
+percentBroken = totalReceivedBroken / totalReceived * 100;//Calculates the percentage of devices received that are broken
 
-//Calculate the percentage of broken devices
-count = 0;
-while(count < numDays)
-{
+//Output
+console.log("The average number of " + deviceName + " received each day over a span of " + numDays + " day(s) is "+ avgReceived + ". \nThese devices had a " + percentBroken + "% fail rate."); //Outputs the desired information to the user
 
-}
-
-
-
-
-/*console.log(deviceName);
-count = 0;
-while(count < numDays){
-	console.log(numReceived[count] + "  " + numReceivedBroken[count] + "\n");
-	count++;
-}*/
 
