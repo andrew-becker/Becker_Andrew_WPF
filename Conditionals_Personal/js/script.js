e5f74ac3762f997e33b50e855f604c7fe4f8d469
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-17 12:44:59 -0400

    updated script.js
    Updated the script.js file with the variables needed

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,4 +1,15 @@
 /* Name: Andy Becker
-   Date: 
-   Assignment:
+   Date: 6/20/13
+   Assignment: Conditionals: Personal
 */
+
+//This program allows the user to input the diameter of their weber charcoal grill and whether they are using indirect or direct heat.  This will calculate how many briquettes will be needed to cook the food.
+//Direct cooking = multiply by 4.5
+//Indirect cooking = multiply by 3
+
+//Variables
+var userDiameter; //diameter of the user's grill.  Will be input by user
+var typeOfCooking; //type of cooking (direct or indirect).  Will be input by user
+var coefficient; //will be used to multiply by the diameter of the grill to get the number of briquettes needed
+var numBriquettes; //Will hold the result for the user
+
