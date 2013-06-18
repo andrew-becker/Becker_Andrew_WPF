d7f83cdd154c97f26b2e10d94a0499645630a20a
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-18 18:36:44 -0400

    updated index.html and script.js
    Updated both files for this assignment

--- Conditionals/Conditionals_Industry/js/script.js
+++ Conditionals/Conditionals_Industry/js/script.js
@@ -1,4 +1,7 @@
 /* Name: Andy Becker
-   Date: 
-   Assignment:
+   Date: 6/20/13
+   Assignment: Conditionals - Industry
 */
+
+//This program will prompt the user to input 3 pieces of infomation to calculate the file size of an uncompressed image.  This can be useful to get a rough idea of how large the file for an image on a website could be.
+
