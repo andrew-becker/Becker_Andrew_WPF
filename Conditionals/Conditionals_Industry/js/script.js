accb4a1615a6e564f2ca491223b8ebe94e8edabe
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-18 18:43:22 -0400

    Updated script.js
    updated with the variables and comments

--- Conditionals/Conditionals_Industry/js/script.js
+++ Conditionals/Conditionals_Industry/js/script.js
@@ -3,5 +3,11 @@
    Assignment: Conditionals - Industry
 */
 
-//This program will prompt the user to input 3 pieces of infomation to calculate the file size of an uncompressed image.  This can be useful to get a rough idea of how large the file for an image on a website could be.
+//This program will prompt the user to input 3 pieces of infomation to calculate the file size of an uncompressed image.  This can be useful to get a rough idea of how large the file for an image on a website could be.  This will multiply the length, width, and bitdepth of the image.  It will then divide that number by 8192 to give the answer in KB.
 
+//Variables
+var horizontalPixels;//User inputted, holds the number of horizontal pixels
+var vertPixels;//User inputted, holds the number of vertical pixels
+var bitDepth;//User inputted, holds the bit depth of the image
+const kbConvert = 8192;//Will be used in the calculations to convert the file size to kilobytes
+var fileSize;//holds the size (in KB) of the file, will be used in the output
