ea1eaf6ac376bd40ddf120c11b89ca00b31878a0
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-19 17:24:53 -0400

    initial setup of files

--- Conditionals/Conditionals_Wacky/js/script.js
+++ Conditionals/Conditionals_Wacky/js/script.js
@@ -1,4 +1,6 @@
 /* Name: Andy Becker
-   Date: 
-   Assignment:
+   Date: 6/20/2013
+   Assignment: Conditionals - Wacky
 */
+
+//This program will prompt the user to enter the par and their score for a 9 hole game of golf.  It will diplay their score and how much they are +/- par
