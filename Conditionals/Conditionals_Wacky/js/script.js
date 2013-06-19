41f8816db5550e9fa97e21a2be3aeaa12b432cee
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-19 17:30:11 -0400

    updated script.js
    Added the variables to the script

--- Conditionals/Conditionals_Wacky/js/script.js
+++ Conditionals/Conditionals_Wacky/js/script.js
@@ -4,3 +4,10 @@
 */
 
 //This program will prompt the user to enter the par and their score for a 9 hole game of golf.  It will diplay their score and how much they are +/- par
+
+//Variables
+var parForHole; //Variable to hold the par for the current hole
+var parForCourse; //Holds the par for this course, will be totaled each hole
+var playerScore; //Holds the players score, will be totaled each hole
+var holeNumber = 1; //Counter to hold the hole holeNumber
+var plusOrMinus; //Variable to hold the + or - symbol
