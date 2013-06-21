8ccd5cdc3fc56e6473cdc7c1363939b6dcd4e23d
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-21 13:19:56 -0400

    Updated Stung! program
    Created the Stung! program

--- Functions/Functions_Worksheet/Stung!/js/script.js
+++ Functions/Functions_Worksheet/Stung!/js/script.js
@@ -1,4 +1,22 @@
 /* Name: Andy Becker
-   Date: 
-   Assignment:
+   Date: 6/21/13
+   Assignment: Functions Worksheet - Stung!
 */
+
+//Calculate the number a bee stings it would take to kill an animal based on the weight
+
+var animal = "deer";
+var animalWeight = 85;
+var stings;
+
+stings = calcStings(animalWeight);
+
+console.log("It would take " + stings + " bee stings to kill a " + animal);
+
+//Function to calculate the number of stings needed based off of the weight of the animal
+
+function calcStings(lbs)
+{
+	const stingsPerLb = 8.666666667;
+	return stingsPerLb * lbs;
+}
