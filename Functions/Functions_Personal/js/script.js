f5dcf9ba1a09587b7d5ec279232c27206a82ec79
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-24 18:28:47 -0400

    updated script.js file
    updated script with an input section.  Also fixed the calcCost function
    to reflect the variables as floats.

--- Functions/Functions_Personal/js/script.js
+++ Functions/Functions_Personal/js/script.js
@@ -10,7 +10,7 @@
 	var calcCost = function(width, length, costPerSqFt, installFees)
 	{
 		var totalCost;
-		totalCost = width * length * costPerSqFt * installFees;
+		totalCost = parseFloat(width) * parseFloat(length) * parseFloat(costPerSqFt) + parseFloat(installFees);
 		return totalCost;
 	}
 	var roomWidth;
@@ -18,11 +18,61 @@
 	var carpetCost;
 	var fees;
 	var costForRoom;
+	var inputCheck;
 
 //input
-	//have a function for input validation that returns true or false....
+	do
+	{
+		roomWidth = prompt("Enter the width of the room in feet");
+		inputCheck = inputValidation(roomWidth);
+		if (inputCheck == 0)
+		{
+			alert("Invalid input!.\nInput must be a number larger than 0.");
+		}
+	}while(inputCheck == 0);
+	do
+	{
+		roomLength = prompt("Enter the length of the room in feet");
+		inputCheck = inputValidation(roomLength);
+		if (inputCheck == 0)
+		{
+			alert("Invalid input!.\nInput must be a number larger than 0.");
+		}
+	}while(inputCheck == 0);
+	do
+	{
+		carpetCost = prompt("Enter the cost of the carpet per square feet.");
+		inputCheck = inputValidation(carpetCost);
+		if (inputCheck == 0)
+		{
+			alert("Invalid input!.\nInput must be a number larger than 0.");
+		}
+	}while(inputCheck == 0);
+	do
+	{
+		fees = prompt("Enter the cost of installation.");
+		inputCheck = inputValidation(fees);
+		if (inputCheck == 0)
+		{
+			alert("Invalid input!.\nInput must be a number larger than 0.");
+		}
+	}while(inputCheck == 0);
+
 
 //calculations
-
+costForRoom = calcCost(roomWidth, roomLength, carpetCost, fees);
+console.log(costForRoom);
 
 //output
+
+function inputValidation(value)
+{
+	if (value <= 0 || isNaN(value) == true)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
