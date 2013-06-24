29a1bd08bf1c2be8da612fdf5b0a4afd83911b95
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-24 12:51:26 -0400

    updated script.js
    Added variables and anonymous function to the script

--- Functions/Functions_Personal/js/script.js
+++ Functions/Functions_Personal/js/script.js
@@ -3,6 +3,26 @@
    Assignment: Functions - Personal
 */
 
-//This program will allow the user to input the number and size of the rooms in their house and will calculate how much carpet they will need.  The user can also input the cost of carpet and installation fees and the program will give them a final cost.
+//This program will allow the user to input the size of a room in their house and will calculate how much carpet they will need.  The user can also input the cost of carpet and installation fees and the program will give them a final cost.
 
 
+//variables
+	var calcCost = function(width, length, costPerSqFt, installFees)
+	{
+		var totalCost;
+		totalCost = width * length * costPerSqFt * installFees;
+		return totalCost;
+	}
+	var roomWidth;
+	var roomHeight;
+	var carpetCost;
+	var fees;
+	var costForRoom;
+
+//input
+	//have a function for input validation that returns true or false....
+
+//calculations
+
+
+//output
