b60d90b50778c25bbc5be642170e0f96addd38d6
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-24 12:33:19 -0400

    Updated index.html and script.js
    updated the title of the html page, and added a desciption of the
    program to the script.js file

--- Functions/Functions_Personal/js/script.js
+++ Functions/Functions_Personal/js/script.js
@@ -1,4 +1,8 @@
 /* Name: Andy Becker
-   Date: 
-   Assignment:
+   Date: 6/27/13
+   Assignment: Functions - Personal
 */
+
+//This program will allow the user to input the number and size of the rooms in their house and will calculate how much carpet they will need.  The user can also input the cost of carpet and installation fees and the program will give them a final cost.
+
+
