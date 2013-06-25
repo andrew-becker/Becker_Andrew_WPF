b8e37237ea146ba7866d58a737c330df1d792353
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-25 18:47:53 -0400

    updated index.html and script.js
    Fixed the title of index.html and added variables to script.js

--- Functions/Functions_Industry/js/script.js
+++ Functions/Functions_Industry/js/script.js
@@ -1,4 +1,53 @@
 /* Name: Andy Becker
-   Date: 
-   Assignment:
+   Date: 6/27/13
+   Assignment: Functions - Industrial
 */
+
+//This function calculates the size of a websites content area and sidebar area and outputs it to the user
+
+//Variables
+var websiteWidth, contentWidth, sidebarWidth;
+
+//Input
+websiteWidth = inputWebsiteWidth();
+
+//Calculations
+contentWidth = calcContentArea(websiteWidth);
+sidebarWidth = calcSidebar(contentWidth, websiteWidth);
+
+//Output
+console.log("Your website is " + websiteWidth + " pixels wide.  The content area should be " + contentWidth + " pixels wide, and the sidebar area should be " + sidebarWidth + " pixels wide.");
+
+//Functions
+function calcContentArea(origSize)
+{
+	var content = parseInt((origSize / 1.618));
+	return content;
+}
+
+function calcSidebar(contentArea, originalSize)
+{
+	var sidebar = originalSize - contentArea;
+	return sidebar;
+}
+
+function inputWebsiteWidth()
+{
+	var width;
+	var validInput = true;
+	do{
+	width = prompt("Enter the width of your website in pixels:");
+	if(isNaN(width == true))
+	{
+		alert("You must input a number.  Example: 1024");
+		validInput = false;
+	}
+	else if(width <= 0)
+	{
+		alert("Your number must be greater than 0")
+		validInput = false;
+	}
+	else validInput = true;
+	}while(validInput == false);
+	return width;
+}
