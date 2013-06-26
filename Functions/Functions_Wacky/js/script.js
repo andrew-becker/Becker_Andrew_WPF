00fa6da766bdc94fe3fb2cc434cf73a43830fd94
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-26 18:43:46 -0400

    updated script.js
    added the variables to the script

--- Functions/Functions_Wacky/js/script.js
+++ Functions/Functions_Wacky/js/script.js
@@ -5,3 +5,12 @@
 
 //This program allows the user to enter the points possible and the points earned for each assignment.  It will calculate the final grade for the class and give them their letter grade and average.
 
+var arrayIndex = 0;
+var moreGrades = "NO";
+var pointsEarned = new Array();
+var pointsPossible = new Array();
+var message;
+var totalPointsEarned;
+var totalPointsPossible;
+
+
