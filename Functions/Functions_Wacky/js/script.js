aa47afda16b8281301b855555bf8b882c498d389
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-26 18:43:21 -0400

    updated index.html and script.js
    initial setup of files

--- Functions/Functions_Wacky/js/script.js
+++ Functions/Functions_Wacky/js/script.js
@@ -1,4 +1,7 @@
 /* Name: Andy Becker
-   Date: 
-   Assignment:
+   Date: 6/27/2013
+   Assignment: Functions - Wacky
 */
+
+//This program allows the user to enter the points possible and the points earned for each assignment.  It will calculate the final grade for the class and give them their letter grade and average.
+
