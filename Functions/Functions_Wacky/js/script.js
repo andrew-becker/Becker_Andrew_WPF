c68d64b2c5d0bc8e005f6e4df56767bb8b12f427
pbplaya87 <Andy@Andrews-MacBook-Pro.local>
2013-06-27 18:03:48 -0400

    updated script.js
    Added more comments to make the code easy to understand.
    
    Changed the calcGradePercent function to only require 2 parameters thus
    making it more reusable

--- Functions/Functions_Wacky/js/script.js
+++ Functions/Functions_Wacky/js/script.js
@@ -5,8 +5,6 @@
 
 //This program allows the user to enter the points possible and the points earned for each assignment.  It will calculate the final grade for the class and give them their letter grade and average.  This program assumes grades are not weighted
 
-var arrayIndex = 0;//sets counter to 0
-var moreGrades = "NO";//initializes variable to hold user input for if they have more grades to enter to "NO"
 var pointsEarned = new Array();//array to hold the points earned for each grade
 var pointsPossible = new Array();//array to hold the points possible for each grade
 var gradeLetter;//holds the letter grade, a+, a, b+...etc
@@ -18,35 +16,34 @@ alert("For this program you will enter the points earned \nfor an assignment and
 do{//do while to take input from the user
 	pointsEarned[pointsEarned.length] = inputNum("Enter the points earned for an assignment.");//passes an input statement to the inputNum function, then sets the input equal to the array using the array length as the index....this array holds the points earned for each assignment
 	pointsPossible[pointsPossible.length] = inputNum("Enter the points possible for the same assignment.");//passes an input statement to the inputNum function, then sets the input equal to the array using the array length as the index...this array holds the points possible for each assignment
-	//moreGrades = inputYesNo();
-}while(inputYesNo() == "YES");
+}while(inputYesNo() == "YES");//checks to see if the function returns YES, if so the do while loop continues
 
-gradePercent = calcGradePercent(pointsEarned, pointsPossible, pointsEarned.length);
-gradeLetter = findGradeLetter(gradePercent);
+gradePercent = calcGradePercent(pointsEarned, pointsPossible);//passes 3 values to the calcGradePercent function.  Passes the pointsEarned array, pointsPossible array, and the length of these arrays.  This function will return the final percent grade for this class.
+gradeLetter = findGradeLetter(gradePercent);//Passes the final grade percent to the findGradeLetter function, this function will return the letter grade for the class
 
-if(gradeLetter == "A" || gradeLetter == "A+")
+if(gradeLetter == "A" || gradeLetter == "A+")//Checks to see if the grade letter is an A or A+, this is used for output formatting reasons only
 {
-	console.log("You earned an " + gradeLetter + " for this class with a " + parseInt(gradePercent) + "%.");
+	console.log("You earned an " + gradeLetter + " for this class with a " + parseInt(gradePercent) + "%.");//Output message for the user, includes both their grade letter and their percentage in the message.  The percentage is parsed as an integer since going to a decimal point isn't needed for the final grade.
 }
-else
+else//If the user's grade is not an A or A+ the following message is displayed
 {
-	console.log("You earned a " + gradeLetter + " for this class with a " + parseInt(gradePercent) + "%.");
+	console.log("You earned a " + gradeLetter + " for this class with a " + parseInt(gradePercent) + "%.");//Output message for the user, includes both their grade letter and their percentage in the message.  The percentage is parsed as an integer since going to a decimal point isn't needed for the final grade.
 }
 
 
-
-function calcGradePercent(earned, possible, arrayLength)
+function calcGradePercent(earned, possible)//Function to calculate a percentage when passed 2 arrays both holding the same amount of numbers.  This function will also work if passing only a single number and not an array
 {
 	var finalPercentage = 0;
-	var gradePercentage = new Array();
-	for(i=0; i < arrayLength; i++)
+	var gradePercentage = new Array(earned.length);
+	for(i=0; i < earned.length; i++)
 	{
 		gradePercentage[i] = parseFloat(earned[i]) / parseFloat(possible[i]) * 100;
 		finalPercentage = parseFloat(gradePercentage[i]) + parseFloat(finalPercentage);
 	}
-	finalPercentage = parseFloat(finalPercentage) / parseInt(arrayLength);
+	finalPercentage = parseFloat(finalPercentage) / parseInt(earned.length);
 	return finalPercentage;
 }
+
 
 function inputNum(str)
 {
@@ -61,6 +58,7 @@ function inputNum(str)
 	return num;
 }
 
+
 function inputValidation(value)//This function validates the user input for number values
 {
 	if (value < 0 || isNaN(value) == true)//checks to see if the user's input is less than 0 or is not a number
@@ -72,6 +70,7 @@ function inputValidation(value)//This function validates the user input for numb
 		return false;//returns false if the input is valid
 	}
 }
+
 
 function inputYesNo(){
 	var ans;
@@ -85,6 +84,7 @@ function inputYesNo(){
 	}while(ans != "YES" && ans != "NO");
 	return ans;
 }
+
 
 function findGradeLetter(gradePercent)
 {
